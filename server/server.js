96ab61fb2267def469f4391fe63df0eb6ef2abf1
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-01-30 08:05:16 +0200

    add jwt

--- server/server.js
+++ server/server.js
@@ -1,15 +1,36 @@
 import express from 'express';
+import jwt from 'jsonwebtoken'	
 
 const app = express();
 
-app.get('',  (req, res) => {
-	"Cook assistants"
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/',  (req, res) => {
+	res.send("Chef assistants");
 });
 
-app.listen('4000', (err) => {
+app.post('/auth/login', (req, res) =>{
+	console.log(req.body);
+
+	const token = jwt.sign({
+		email: req.body.email,
+		password: req.body.password
+	}, 'sekretKey');
+	
+	res.json({
+		success: true,
+		token
+	})
+
+} )
+
+app.listen(PORT, (err) => {
 	if (err) {
 		return console.log(err);
 	}
+});
 
-	console.log('Server started');
-})
+	console.log('Server started on PORT ' + PORT);
+	
