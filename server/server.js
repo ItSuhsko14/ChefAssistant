10c7b0288ffc4eb780afce2f2a80eacd314077d7
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-09-22 17:08:35 +0300

    change router.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import { cardCreateValidation, registerValidation, loginValidation } from './validation/validation.js'
 import mongoose from 'mongoose'
-
 import { checkAuth, handleValidationError } from './utils/index.js'
-
-
 import { UserController, CardController } from './controllers/index.js'
-
 import cors from 'cors'
 
 mongoose.set('strictQuery', false);
@@ -50,29 +46,6 @@ app.listen(PORT, (err) => {
 });
 
 	console.log('Server started on PORT ' + PORT);
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD-9zY5NAFS_dTA1dlbZXAwh1DwRlWWIfE",
-  authDomain: "cheff-173de.firebaseapp.com",
-  projectId: "cheff-173de",
-  storageBucket: "cheff-173de.appspot.com",
-  messagingSenderId: "487205944495",
-  appId: "1:487205944495:web:56db277958a946cc05685a",
-  measurementId: "G-PXF2BYHGP2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 
 
